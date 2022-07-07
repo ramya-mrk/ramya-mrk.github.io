f2110f995d521d6634964153386b1918faff49e5
ramya-mrk <108858408+ramya-mrk@users.noreply.github.com>
2022-07-07 23:24:06 +0530

    Sort Names

--- sort_name.js
+++ sort_name.js
@@ -1,12 +1,9 @@
 function sort(){
    var n1=document.getElementById('name1').value;
-   var n2=document.getElementById('name2').value;
-   console.log(n1.length);
-   console.log(n2.length);
-   
+   var n2=document.getElementById('name2').value;  
    if (n2.length > n1.length){
-	   document.getElementById('name1').value=var n2;
-	   document.getElementById('name2').value=var n1;
+	   document.getElementById('name1').value=n2;
+	   document.getElementById('name2').value=n1;
 	  
    }	   
 }
@@ -16,8 +13,15 @@ function ascend(){
 	var a2=document.getElementById('name2').value;
 
     if(a1.length>a2.length){
-	   document.getElementById('name1').value=var a2;
-	   document.getElementById('name2').value=var a1;
+	   document.getElementById('name1').value=a2;
+	   document.getElementById('name2').value=a1;
 	}
 }
    
+function rev_both(){
+	var a =document.getElementById('name1').value; 
+	var b =document.getElementById('name2').value;
+	document.getElementById('name2').value= a;  
+	document.getElementById('name1').value=b;
+}
+
