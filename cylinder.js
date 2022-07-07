eb212c259faefdd31d1be686d90b3740c7104feb
ramya-mrk <108858408+ramya-mrk@users.noreply.github.com>
2022-07-07 14:00:11 +0530

    Cylinder

---
+++ cylinder.js
@@ -0,0 +1,8 @@
+function area(){
+    var r= document.getElementById('t1').value;
+	var h= document.getElementById('t2').value;
+	var a= 3.14*parseInt(r)*parseInt(r)*parseInt(h);
+	document.getElementById('t3').value=a;
+	var volume= 3.14* (parseInt(r)**2) * parseInt(h);
+	document.getElementById('t4').value=volume;
+}
