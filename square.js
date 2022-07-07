191320edcfdac71e8c3b5d4674fd17e595663e3d
ramya-mrk <108858408+ramya-mrk@users.noreply.github.com>
2022-07-07 23:00:49 +0530

    Delete square.js

--- square.js
+++
@@ -1,7 +0,0 @@
-function area(){
-    var side= document.getElementById('t1').value;
-	var a= parseInt (side)**2;
-	document.getElementById('t2').value=a;
-	var peri= 4 * parseInt(side);
-	document.getElementById('t3').value=peri;
-}
