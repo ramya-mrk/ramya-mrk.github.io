77024f8d63e791498db16173f71eae250c7d297e
ramya-mrk <108858408+ramya-mrk@users.noreply.github.com>
2022-07-07 22:46:38 +0530

    Area,Volume - Cylinder

--- cylinder.js
+++ cylinder.js
@@ -1,7 +1,7 @@
 function area(){
     var r= document.getElementById('t1').value;
 	var h= document.getElementById('t2').value;
-	var a= 3.14*parseInt(r)*parseInt(r)*parseInt(h);
+	var a= 2*3.14*parseInt(r)*parseInt(h);
 	document.getElementById('t3').value=a;
 	var volume= 3.14* (parseInt(r)**2) * parseInt(h);
 	document.getElementById('t4').value=volume;
