bf58310f2c91e7a523104c4e0600144d19b91bd9
ramya-mrk <108858408+ramya-mrk@users.noreply.github.com>
2022-07-07 14:03:09 +0530

    Delete rectangle.js

--- rectangle.js
+++
@@ -1,8 +0,0 @@
-function area(){
-    var l= document.getElementById('t1').value;
-	var b= document.getElementById('t2').value;
-	var a= parseInt(l)*parseInt(b);
-	document.getElementById('t3').value=a;
-	var peri= 2 * (parseInt(l)+parseInt(b));
-	document.getElementById('t4').value=peri;
-}
