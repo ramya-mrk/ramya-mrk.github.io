fc02acfa05ab358be6b0100e1c623fe37e46c7aa
ramya-mrk <108858408+ramya-mrk@users.noreply.github.com>
2022-07-07 13:58:09 +0530

    Delete cylinder.js

--- cylinder.js
+++
@@ -1,8 +0,0 @@
-function area(){
-    var r= document.getElementById('t1').value;
-	var h= document.getElementById('t2').value;
-	var a= 3.14*parseInt(r)*parseInt(r)*parseInt(h);
-	document.getElementById('t3').value=a;
-	var volume= 3.14*parseInt(r)**2 * parseInt(h);
-	document.getElementById('t4').value=volume;
-}
