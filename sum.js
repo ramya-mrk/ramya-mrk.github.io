6816a709558d78e8d593bda14d4fd2beaabea71a
ramya-mrk <108858408+ramya-mrk@users.noreply.github.com>
2022-07-07 15:29:17 +0530

    Delete sum.js

--- sum.js
+++
@@ -1,7 +0,0 @@
-
-function addition(){
-    var n1 = document.getElementById('t1').value;
-    var n2 = document.getElementById('t2').value;
-    var sum = parseInt(n1)+parseInt(n2);
-    document.getElementById('t3').value=sum;
-}
